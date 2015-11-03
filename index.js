a56e0a6a8e0764ed32bfcd7fd9b2c7c5b381fb86
Jeff Jewiss <jeffjewiss@gmail.com>
2015-11-02 19:21:50 -0500

    Allow files other than app.css to be processed

--- index.js
+++ index.js
@@ -1,33 +1,44 @@
 var PostcssCompiler = require('broccoli-postcss');
+var path = require('path');
+var merge = require('merge');
+var mergeTrees = require('broccoli-merge-trees');
 var checker = require('ember-cli-version-checker');
 
-// PostCSSPlugin constructor
-function PostCSSPlugin (options) {
+function PostcssPlugin(optionsFn) {
   this.name = 'ember-cli-postcss';
-  this.options = options;
-  this.plugins = options.plugins;
-  this.map = options.map;
+  this.optionsFn = optionsFn;
 }
 
-PostCSSPlugin.prototype.toTree = function (tree, inputPath, outputPath) {
-  var trees = [tree];
+PostcssPlugin.prototype.toTree = function (tree, inputPath, outputPath, inputOptions) {
+  var inputTrees = [tree];
+  var options = merge({}, this.optionsFn, inputOptions);
 
-  if (this.options.includePaths) {
-    trees = trees.concat(this.options.includePaths);
+  if (options.includePaths) {
+    inputTrees = inputTrees.concat(this.options.includePaths);
   }
 
-  inputPath += '/' + this.options.inputFile;
-  outputPath += '/' + this.options.outputFile;
+  var plugins = options.plugins;
+  var map = options.map;
 
-  return new PostcssCompiler(trees, inputPath, outputPath, this.plugins, this.map);
+  var ext = options.extension || 'css';
+  var paths = options.outputPaths;
+  var trees = Object.keys(paths).map(function(file) {
+    var input = path.join(inputPath, file + '.' + ext);
+    var output = paths[file];
+    return new PostcssCompiler(inputTrees, input, output, plugins, map);
+  });
+
+  return mergeTrees(trees);
 };
 
 module.exports = {
   name: 'Ember CLI Postcss',
+
   shouldSetupRegistryInIncluded: function() {
     return !checker.isAbove(this, '0.2.0');
   },
-  included: function included (app) {
+
+  included: function included(app) {
     this.app = app;
     // Initialize options if none were passed
     var options = app.options.postcssOptions || {};
@@ -39,7 +50,7 @@ module.exports = {
     options.outputFile = options.outputFile || this.project.name() + '.css';
 
     // Add to registry and pass options
-    app.registry.add('css', new PostCSSPlugin(options));
+    app.registry.add('css', new PostcssPlugin(options));
 
     if (this.shouldSetupRegistryInIncluded()) {
       this.setupPreprocessorRegistry('parent', app.registry);
