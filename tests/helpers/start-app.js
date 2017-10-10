d5ff60091e0e6067c01be7a298ac8394ea369edc
Jeff Jewiss <jeffjewiss@gmail.com>
2017-10-10 12:09:37 +0100

    fix imports

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -1,7 +1,7 @@
 import Application from '../../app'
 import config from '../../config/environment'
-import run from 'ember-runloop'
-import { assign } from 'ember-platform'
+import { run } from '@ember/runloop'
+import { assign } from '@ember/polyfills'
 
 export default function startApp (attrs) {
   let application
