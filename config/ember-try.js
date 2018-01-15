1d0cdf92f94af6c9ef80fffc6b990d46c319987f
Jeff Jewiss <jeff@jeffjewiss.com>
2018-01-15 17:26:39 +0000

    Remove beta and canary for now

--- config/ember-try.js
+++ config/ember-try.js
@@ -52,38 +52,6 @@ module.exports = {
           'ember-source': null
         }
       }
-    },
-    {
-      name: 'ember-beta',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#beta'
-        },
-        resolutions: {
-          'ember': 'beta'
-        }
-      },
-      npm: {
-        devDependencies: {
-          'ember-source': null
-        }
-      }
-    },
-    {
-      name: 'ember-canary',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#canary'
-        },
-        resolutions: {
-          'ember': 'canary'
-        }
-      },
-      npm: {
-        devDependencies: {
-          'ember-source': null
-        }
-      }
     }
   ]
 }
