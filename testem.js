1aa3e418ee8ab9e43b96041376fcf687cd287884
Jeff Jewiss <jeff@jeffjewiss.com>
2018-01-25 14:23:14 +0000

    Fix CI tests

--- testem.js
+++ testem.js
@@ -1,12 +1,28 @@
+/* eslint-env node */
 module.exports = {
-  'framework': 'qunit',
-  'test_page': 'tests/index.html?hidepassed',
-  'disable_watching': true,
-  'launch_in_ci': [
-    'PhantomJS'
-  ],
-  'launch_in_dev': [
-    'PhantomJS',
+  test_page: 'tests/index.html?hidepassed',
+  disable_watching: true,
+  launch_in_ci: [
     'Chrome'
-  ]
+  ],
+  launch_in_dev: [
+    'Chrome'
+  ],
+  browser_args: {
+    Chrome: {
+      mode: 'ci',
+      args: [
+        /*
+          --no-sandbox is needed when running Chrome inside a container.
+          See https://github.com/ember-cli/ember-cli-chai/pull/45/files.
+        */
+        process.env.TRAVIS ? '--no-sandbox' : null,
+
+        '--disable-gpu',
+        '--headless',
+        '--remote-debugging-port=9222',
+        '--window-size=1440,900'
+      ].filter(Boolean)
+    }
+  }
 }
