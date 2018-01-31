127d9446467e47c75148ffcff28e8a60bed85ed0
Jeff Jewiss <jeff@jeffjewiss.com>
2018-01-31 23:45:57 +0000

    display the version number in docs

---
+++ tests/dummy/app/controllers/application.js
@@ -0,0 +1,11 @@
+import Controller from '@ember/controller'
+import config from '../config/environment'
+
+const versionRegExp = /\d+[.]\d+[.]\d+/
+const {
+  APP: { version }
+} = config
+
+export default Controller.extend({
+  addonVersion: version.match(versionRegExp)[0]
+})
