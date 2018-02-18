946e4cdc8875c03c6379ed1359c1cdf6766cfbcc
Jeff Jewiss <jeff@jeffjewiss.com>
2018-02-18 19:45:49 +0000

    use new testing api

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,16 +1,16 @@
-import Router from 'ember-router'
+import Router from '@ember/routing/router'
 import RouterScroll from 'ember-router-scroll'
 import config from './config/environment'
 
-const MyRouter = Router.extend(RouterScroll, {
+const EmberRouter = Router.extend(RouterScroll, {
   location: config.locationType,
   rootURL: config.rootURL
 })
 
-MyRouter.map(function () {
+EmberRouter.map(function () {
   this.route('test')
   this.route('docs')
   this.route('library')
 })
 
-export default MyRouter
+export default EmberRouter
