85304b5eabde97b32774f9f87b7119ab8c006a79
Jeff Jewiss <jeff@jeffjewiss.com>
2020-01-27 15:03:04 -0500

    allow use of native class for dummy app router

--- tests/.eslintrc.js
+++ tests/.eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      legacyDecorators: true,
+    },
+  },
   env: {
     embertest: true
   }
