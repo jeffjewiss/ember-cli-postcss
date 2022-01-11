386eb70ad0970a40295ef1788a2e641fef1ac075
Jeff Jewiss <jeff@jeffjewiss.com>
2022-01-10 20:16:38 -0500

    fix qunit setup

--- tests/dummy/app/routes/docs.js
+++ tests/dummy/app/routes/docs.js
@@ -1,15 +1,3 @@
 import Route from '@ember/routing/route'
-import { scheduleOnce } from '@ember/runloop'
-import { inject } from '@ember/service'
 
-export default Route.extend({
-  codePrettify: inject(),
-
-  activate () {
-    scheduleOnce('afterRender', this, this.setupPrettify)
-  },
-
-  setupPrettify () {
-    this.codePrettify.prettify()
-  }
-})
+export default Route.extend({})
